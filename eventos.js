59bdde1c5977de46f360cc2a26940f6da81d1682
juanmaa14 <juanmtesting@gmail.com>
2025-08-16 20:34:31 -0300

    arreglo

--- eventos.js
+++ eventos.js
@@ -1,6 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
     const div = document.querySelector('div');
+    const button = div.querySelector('button');
+
     div.addEventListener('click', function() {
         alert('Hola! Soy el div');
+    });
+
+    button.addEventListener('click', function(event) {
+        event.stopPropagation();
     });
 });
